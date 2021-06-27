7fd4995e575db07d2e4184ec8d22fb80a7349d1b
aditi jha <68181276+aditi944@users.noreply.github.com>
2021-06-27 13:52:53 +0530

    Create wave traversal in 2D array.cpp

---
+++ array/wave traversal in 2D array.cpp
@@ -0,0 +1,11 @@
+void wave_array(int arr[]){
+  for(int i=0;i<arr.length();i++){
+    if(j%2==0){
+    for(int j=0;j<arr.length();j++){
+    cout<<arr[i][j];}
+    }
+    else{
+      for(int j=arr.length();j>0;j--){
+    cout<<arr[i][j];}
+    }
+    }
