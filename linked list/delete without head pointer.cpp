d8537678dd117e1fd7bdb78304f8df1bc667563a
aditi jha <68181276+aditi944@users.noreply.github.com>
2021-06-28 13:29:15 +0530

    Create delete without head pointer.cpp

---
+++ linked list/delete without head pointer.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+    //Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *del)
+    {
+        if (del == NULL) // If linked list is empty
+        return;
+    else {
+ 
+        if (del->next == NULL) {
+            printf("This is last node, require head, can't "
+                   "be freed\n");
+            return;
+        }
+ 
+        struct Node* temp = del->next;
+ 
+        // Copy data of the next node to current node
+        del->data = del->next->data;
+ 
+        // Perform conventional deletion
+        del->next = del->next->next;
+ 
+        free(temp);// Your code here
+    }
+    }
+
+};
