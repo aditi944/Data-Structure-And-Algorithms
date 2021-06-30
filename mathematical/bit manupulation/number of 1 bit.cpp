e6668cdcf1050a51989aa5123a6c3dc88f7b6ceb
aditi jha <68181276+aditi944@users.noreply.github.com>
2021-06-30 12:05:19 +0530

    Update number of 1 bit.cpp

--- mathematical/bit manupulation/number of 1 bit.cpp
+++ mathematical/bit manupulation/number of 1 bit.cpp
@@ -9,3 +9,17 @@ public:
     return count;
 }
 };
+
+//lightweight solution
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+   int count=0;
+    while(n!=0)
+    {
+        n=n & (n-1);
+        count++;
+    }
+    return count;
+    }
+};
