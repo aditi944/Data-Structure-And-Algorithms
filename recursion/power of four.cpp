88a04bf066c9a2580bf23b2d90ae3d27415d9687
aditi jha <68181276+aditi944@users.noreply.github.com>
2021-07-01 15:09:26 +0530

    Create power of four.cpp

---
+++ recursion/power of four.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        
+        if(n==1)
+        {
+            return true;
+        }
+        if(n%4!=0 || n<=0)
+        {
+            return false;
+        }
+        return isPowerOfFour(n/4);
+        
+    }
+};
