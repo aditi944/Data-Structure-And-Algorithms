9096b816dfa90dba558c983a087e148221b53c8d
aditi jha <68181276+aditi944@users.noreply.github.com>
2021-07-06 16:00:45 +0530

    Create minimum depth.cpp

---
+++ trees/minimum depth.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+       
+        if (root == nullptr)                                         
+            return 0;
+        else if (root->left == nullptr && root->right != nullptr)    
+            return 1 + minDepth(root->right);
+        else if (root->left != nullptr && root->right == nullptr)    
+            return 1 + minDepth(root->left);
+        else                                                         
+            return 1 + min(minDepth(root->left), minDepth(root->right));
+    }
+};
