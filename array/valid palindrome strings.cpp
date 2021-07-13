7659bbe453caecefa39e54ac3f4f2b0724a15c0e
aditi jha <68181276+aditi944@users.noreply.github.com>
2021-07-13 21:20:45 +0530

    Create valid palindrome strings.cpp

---
+++ array/valid palindrome strings.cpp
@@ -0,0 +1,23 @@
+//Input: s = "A man, a plan, a canal: Panama"
+//Output: true
+//Explanation: "amanaplanacanalpanama" is a palindrome.
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.length()<=1){
+
+            return true;
+        }
+        int st=0,en=s.length()-1;
+        while(st<en){
+
+            while(st<en&&!isalnum(s[st]))st++;
+            while(st<en&&!isalnum(s[en]))en--;
+            if(st<en&&tolower(s[st])!=tolower(s[en]))
+                return false;
+            st++;
+            en--;
+        }
+        return true;
+    }
+};
