cf0104b812ba8b523465fab100736783f590d5b8
Aditi Jha <68181276+aditi944@users.noreply.github.com>
2021-07-30 14:29:02 +0530

    Update detect_cycle.cpp

--- linked list/detect_cycle.cpp
+++ linked list/detect_cycle.cpp
@@ -10,6 +10,9 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
      
+         if(head==NULL){
+        return false;
+    }
       
       struct ListNode*fast,*slow;
       fast=head;
