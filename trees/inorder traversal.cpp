37b21777849d6250e436091aa0516261a5825d0f
Aditi Jha <68181276+aditi944@users.noreply.github.com>
2021-07-31 15:59:53 +0530

    Update inorder traversal.cpp

--- trees/inorder traversal.cpp
+++ trees/inorder traversal.cpp
@@ -62,3 +62,46 @@ class Node {
     }
 
 };
+
+
+//using stack
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+   vector<int>ans;
+        stack<TreeNode*>s;
+        while(1){
+            
+
+            while(root){
+                s.push(root);            
+                    root = root->left;
+            
+            }
+            if(s.empty())
+                break;
+            
+            root = s.top();
+            s.pop();
+            
+            ans.push_back(root->val);
+            
+            root = root->right;
+        }
+        
+        return ans;
+        
+    }
+};
