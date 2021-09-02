dbbaecc96e38da9c9864f4f23728fecfccaf0969
Aditi <68181276+aditi944@users.noreply.github.com>
2021-09-02 19:38:14 +0530

    Update median from data stream.cpp

--- heap/median from data stream.cpp
+++ heap/median from data stream.cpp
@@ -98,5 +98,5 @@ double findMedian() {
  * Your MedianFinder object will be instantiated and called as such:
  * MedianFinder* obj = new MedianFinder();
  * obj->addNum(num);
- * double param_2 = obj->findMedian();
+ * double param_2 = obj->findMedian();;
  */
