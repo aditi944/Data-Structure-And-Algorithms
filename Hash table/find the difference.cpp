a070db7f95e2302f9682cfcbb932b82e7b4813e3
Aditi <68181276+aditi944@users.noreply.github.com>
2021-10-01 21:10:34 +0530

    Create find the difference.cpp

---
+++ Hash table/find the difference.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/find-the-difference/
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int>mp;
+        for(int i=0;i<t.length();i++){
+            mp[t[i]]++;
+            
+        }
+        for(int i=0;i<s.length();i++){
+            mp[s[i]]--;
+        }
+        for (auto& it : mp) {
+            if(it.second==1)return t[]
+        }
+    }
+};
