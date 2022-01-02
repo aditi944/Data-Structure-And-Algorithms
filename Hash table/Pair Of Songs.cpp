410392972f2e3298dbbe1f749fb4297f24963e4d
Aditi <68181276+aditi944@users.noreply.github.com>
2022-01-02 15:52:55 +0530

    Create Pair Of Songs.cpp

---
+++ Hash table/Pair Of Songs.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
+
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& time) {
+       int ans = 0;
+    vector<int> count(60);
+
+    for (int t : time) {
+      t %= 60;
+      ans += count[(60 - t) % 60];
+      count[t]++;
+    }
+
+    return ans;
+    }
+};
