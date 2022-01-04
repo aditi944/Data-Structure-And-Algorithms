7f781bf8b1c10e5e1ce8962f3369a4732a3c8b55
Aditi <68181276+aditi944@users.noreply.github.com>
2022-01-04 19:56:31 +0530

    Create Next Permutation.cpp

---
+++ array/Next Permutation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i,j;
+        for( i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                break;
+            }
+        }
+        if(i<0){
+            reverse(nums.begin(),nums.end());
+        }
+        else{
+            for(j=nums.size()-1;j>i;j--){
+                if(nums[j]>nums[i])break;
+            }
+        
+            swap(nums[i],nums[j]);
+            reverse(nums.begin()+1+i, nums.end());      
+        }
+        
+    }
+};
